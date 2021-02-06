d097c1937833c15b5b4d4bcf4a80fb4bf75a3750
frosa9876 <77249102+frosa9876@users.noreply.github.com>
2021-02-05 21:49:07 -0500

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,53 +1,47 @@
-// use the same header comments you've used in chapter 1 and chapter 2 program assignments
+// Author: Fernando Rosa
+// Date: 02/05/2021
+// Hours: 15 Hours
+// Class: CSCI - U200
+// Assignment: Program 1
+
 
 import java.util.*;
 
 public class Palindrome {
-  //
-  // method: isPalindrome
-  // pre-conditions: a string is passed in
-  // post-conditions: return true if the string is a palindrome otherwise return false
-  //
+  
+
   public static Boolean isPalindrome(String s){
+    
+    s=s.replaceAll("[^a-zA-Z0-9]", "");
+    String Str = new String();
     stringBuilder sb = new StringBuilder(str);
     sb.reverse();
     string rev = sb.toString();
-    if(str.equals(rev)){
-      return true;
     
+    if(str.equals(rev))
+      return true;
     }else{
       return false;
-    
-    }
-    
     }
   }
 
   public static void main(String[] args) {
-      // define a scanner object to read from System.in
-     //
-    Scanner.scan = new Scanner(System.in);
-      // define a string called word and read the first line from the scanner object
-     //
-    word w = new word(new file)
-      // keep reading words until the word QUIT is read in
-     //
-      while (!word.equals("QUIT")) {
-        //
-        // call the isPalindrome method passing it the word
-       //
-        str = str.replaceAll("[^a-zA-Z0-9]", "");
-        return str.trim().equalsIgnoreCase(new StringBuilder(str.trim()).reverse().toString() );
-        // based on what this method returns (true or false) output a message
-        //
-        if (isPalindrome(word))
-            System.out.println("the string [" + word + "] IS a palindrome.");
-        else
-        // provide false branch
-          System.out.println("the string [" + word + "] IS NOT a palindrome.");
-           
+  
+    Scanner scan = new Scanner("testA.txt");
+    String word = new String();
+    word = scan.nextLine();
+    
+      while (!word.equals("QUIT")){
         
-        // read the next string
+        if (isPalindrome(word)== true){
+            System.out.println("the string [" + word + "] IS a palindrome.");
+        }
+        else{
+          
+          System.out.println("the string [" + word + "] IS NOT a palindrome.");
+        }
+        
+        word = scan.nextLine();
         
       }
   }
